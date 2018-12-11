28797c9f9e09fe2b51f72878af3e7f18333f365d
github username <maduri.vassaramo@gmail.com>
2018-12-11 15:39:51 +0000

    solved alphabetSoup

---
+++ alphabetSoup.rb
@@ -0,0 +1,8 @@
+def alphabetSoup(string)
+    array = string.split("")
+    array.sort.join("")
+end
+
+# takes in a string parameter 
+# convert string to array to use sort
+# convert array back to string 
