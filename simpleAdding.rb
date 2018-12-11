f11dc10cc4b14094c971b8d59936d2ae68723cf2
github username <maduri.vassaramo@gmail.com>
2018-12-11 09:57:55 +0000

    solved simpleAdding

---
+++ simpleAdding.rb
@@ -0,0 +1,14 @@
+def simpleAdding(num)
+    total = 0
+
+    (1..num).each do |i|
+        total += i
+    end
+    total 
+end
+
+
+
+# create total variable to store count
+# loop through all the numbers and add to total  
+# return total variable 
